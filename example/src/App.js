8ce2cac0287fe33d04852145e80ad438046a8ddf
changxinaglin <281532612@qq.com>
2020-11-12 14:36:54 +0800

    Initial Commit

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,32 @@
-import React from 'react'
+// import React from 'react'
 
-import { ExampleComponent } from 'colored-heading'
-import 'colored-heading/dist/index.css'
+// import { ExampleComponent } from 'colored-heading'
+// import 'colored-heading/dist/index.css'
 
-const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+// const App = () => {
+//   return <ExampleComponent text="Create React Library Example 😄" />
+// }
+
+// export default App
+
+import React, { Component } from 'react'
+
+import { ColoredHeadingOne, ColoredHeadingTwo } from 'colored-heading'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <ColoredHeadingOne
+          text='Colored heading one is working'
+          color={'red'}
+        />
+
+        <ColoredHeadingTwo
+          text="Colored heading two is working"
+          color={'blue'}
+        />
+      </div>
+    )
+  }
 }
-
-export default App
